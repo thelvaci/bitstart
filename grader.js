5b8ece2b2b2048a723061b08df0761efd05d7fbc
thelvaci <helvacitahir@gmail.com>
2013-07-10 18:30:25 +0000

    Updated grader.js

--- grader.js
+++ grader.js
@@ -25,7 +25,7 @@ var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
-var CHECKSFILE_DEFAULT = "checks.json";
+var CHECKSFILE_DEFAULT = "check.json";
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
